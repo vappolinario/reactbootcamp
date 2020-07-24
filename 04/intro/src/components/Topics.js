677da3508e5493e1e28f3b5955e9f919f3732fd5
Vitor B Appolinario <vitorappolinario@gmail.com>
2020-07-24 20:41:13 -0300

    Criar um app com o create-react-app

---
+++ 04/intro/src/components/Topics.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+    Link,
+    Route,
+} from 'react-router-dom'
+
+
+function Topic({ match }) {
+    return <h3>{match.params.topicId}</h3>
+}
+
+export default function Topics( { match }){
+    return (
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                <li><Link to={`${match.url}/rendering`}>Rendering with react</Link></li>
+                <li><Link to={`${match.url}/components`}>Components</Link></li>
+                <li><Link to={`${match.url}/props-v-state`}>Props vs States</Link></li>
+            </ul>
+            <hr />
+            <Route path={`${match.path}/:topicId`} component={Topic} />
+            <Route exact path={`${match.path}/`} render={() => {
+                return <h3>Select a topic</h3>
+            }} />
+        </div>
+    )
+}
